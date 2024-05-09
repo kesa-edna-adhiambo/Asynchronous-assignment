fd2cb427811c2cfb5d567c93edf31a8b31310caa
kesa-edna-adhiambo <edna@gmail.com>
2024-05-09 06:30:46 +0300

    adds file

---
+++ closures.js
@@ -0,0 +1,14 @@
+//Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
+//The function should log the message to the console after the specified delay time.
+function introduction(){
+    console.log('My name is Edna')
+};
+setTimeout(introduction , 4000)
+
+
+//You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given a user ID. 
+//Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
+
+
+
+//
